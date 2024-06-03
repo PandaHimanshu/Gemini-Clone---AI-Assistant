a5008052b31448e04f2a309ac4294efd930a83c4
himanshu <pandahimanshu707@gmail.com>
2024-06-03 07:00:54 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,35 @@
+import code from './code.png'
+import compass from './compass.png'
+import gallery_icon1 from './gallery_icon1.png'
+import gallery_icon2 from './gallery_icon2.png'
+import gemini_icon from './gemini_icon.png'
+import history_icon from './history_icon.png'
+import lightbulb from './lightbulb.png'
+import menu from './menu.png'
+import message_icon from './message_icon.png'
+import mic from './mic.png'
+import plus_sign from './plus_sign.png'
+import question from './question.png'
+import send from './send.png'
+import settings from './settings.png'
+import user_logo from './users.png'
+import youtube from './youtube.png'
+export const assets = {
+    code,
+    compass,
+    gallery_icon1,
+    gallery_icon2,
+    gemini_icon,
+    history_icon,
+    lightbulb,
+    menu,
+    message_icon,
+    mic,
+    plus_sign,
+    question,
+    send,
+    settings,
+    user_logo,
+    youtube
+
+}
